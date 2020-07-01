c7ede5d115632aaee45ed95b84d250f6ba470b58
Vova Ragoulin <vovaragulin@gmail.com>
2020-07-01 22:50:16 +0300

    Начата работа с импортом из Excel

--- Forms/ExcelParser.Designer.cs
+++ Forms/ExcelParser.Designer.cs
@@ -47,7 +47,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(145, 57);
+            this.label2.Location = new System.Drawing.Point(145, 42);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(113, 13);
             this.label2.TabIndex = 3;
@@ -55,7 +55,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 52);
+            this.button1.Location = new System.Drawing.Point(12, 37);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(127, 23);
             this.button1.TabIndex = 4;
@@ -74,9 +74,9 @@
             this.Price_Columnt,
             this.Amount_Column,
             this.Category_Column});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 92);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 66);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(776, 278);
+            this.dataGridView1.Size = new System.Drawing.Size(776, 304);
             this.dataGridView1.TabIndex = 5;
             // 
             // Number_Column
---
+++ Parse.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using WindowsFormsApp1.Forms;
+using Microsoft.Office.Interop.Excel;
+
+namespace WindowsFormsApp1
+{
+    class Parse
+    {
+        private string path;
+        
+
+        Parse(string Path)
+        {
+            path = Path;
+        }
+
+        private void GetDataFromExcel(string pathToFile)
+        {
+            Application xlApp = new Application();
+
+            Workbook xlWorkBook = xlApp.Workbooks.Open(pathToFile, 
+                UpdateLinks: 0, 
+                ReadOnly: true, 
+                Format: 5,
+                Password: "", 
+                WriteResPassword: "", 
+                IgnoreReadOnlyRecommended: false, 
+                Origin: XlPlatform.xlWindows, 
+                Delimiter: "", 
+                Editable: false, 
+                Notify: false, 
+                Converter: 0, 
+                AddToMru: false, 
+                Local: false, 
+                CorruptLoad: false); 
+
+            Worksheet xlWorkSheet = (Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            Range columnRange = xlWorkSheet.UsedRange; 
+            
+                
+        }
+    }
+}
